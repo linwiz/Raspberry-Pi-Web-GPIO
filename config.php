d8ed6ae39037fd072685aad7d6774625b2327c31
linwiz <mtec007@gmail.com>
2014-08-27 12:52:50 -0400

    Reorganized Code

--- config.php
+++ config.php
@@ -6,13 +6,12 @@ $MySQLi_User		= 'user';		// Enter your MySQLi Username here.
 $MySQLi_Password	= 'pass';		// Enter your MySQLi Password here.
 $MySQLi_DataBase	= 'gpio';		// Enter your MySQLi Database here.
 
+$db = new mysqli($MySQLi_Host, $MySQLi_User, $MySQLi_Password, $MySQLi_DataBase);
+
 
 // Raspberry Pi board revision
 // Enter your revision if you know it, OR
 // leave this line default and run setup.py
 // eg; python setup.py
 $pi_rev = '';
-
-
-$db = new mysqli($MySQLi_Host, $MySQLi_User, $MySQLi_Password, $MySQLi_DataBase);
 ?>
