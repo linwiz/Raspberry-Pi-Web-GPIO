713517f8736b443255a822440beff5024d71a584
linwiz <mtec007@gmail.com>
2014-08-27 12:53:33 -0400

    Cleaned up Code

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python
 import RPi.GPIO as GPIO
-a = GPIO.RPI_REVISION
-print "Raspberry Pi Revision Found! Revision: " + str(a)
 
+# Retrieve revision information
+revision = GPIO.RPI_REVISION
+print "Raspberry Pi Revision Found! Revision: " + str(revision)
+
+# Save revision information in the config.php file
 s = open("config.php").read()
-s = s.replace("$pi_rev = '';", "$pi_rev = '" + str(a) + "';")
+s = s.replace("$pi_rev = '';", "$pi_rev = '" + str(revision) + "';")
 f = open("config.php", 'w')
 f.write(s)
 f.close()
