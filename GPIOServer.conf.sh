43a17f2455d997433a5d40f0cd10bfcb8883e72d
Matthew Motz <mtec007@gmail.com>
2015-01-09 09:31:08 -0500

    Added extender config options

--- GPIOServer.conf.sh
+++ GPIOServer.conf.sh
@@ -6,6 +6,20 @@ mysqlusername="user"
 mysqlpassword="pass"
 mysqldatabase="gpio"
 
+
+# Enable extenders
+# MCP23017
+mcp23017a="FALSE"
+mcp23017b="FALSE"
+mcp23017c="FALSE"
+mcp23017d="FALSE"
+mcp23017e="FALSE"
+mcp23017f="FALSE"
+mcp23017g="FALSE"
+mcp23017h="FALSE"
+
+
+
 # Enable logging in MySQL
 # TODO : Doesn't log errors
 logging="TRUE"
