8e638d3fe26e75ea3d0d68fb8ae1df567e7e2982
Matthew Motz <mtec007@gmail.com>
2015-01-09 15:19:24 -0500

    WIP commit; may break functionality

--- GPIOServer.sh
+++ GPIOServer.sh
@@ -25,54 +25,112 @@ addLogItem() {
 addLogItem "Starting GPIO Server"
 trap "addLogItem Stopping GPIO Server" EXIT
 
+mysqlquery="mysql -B --host=$mysqlhostname --disable-column-names --user=$mysqlusername --password=$mysqlpassword $mysqldatabase"
+
 # Retreive all pins.
-pins=`mysql -B --host=$mysqlhostname --disable-column-names --user=$mysqlusername --password=$mysqlpassword $mysqldatabase -e"SELECT pinNumberBCM FROM pinRevision$revision"`
+pins=`echo "SELECT pinNumberBCM FROM pinRevision$revision" | $mysqlquery`
+mcp23017pins=`echo "SELECT pinNumber FROM mcp23017" | $mysqlquery`
 
 # Start Loop.
 while true; do
-	for PIN in $pins ;
-		do
-			# Enable or Disable pins accordingly.
-			enabled[$PIN]=`mysql -B --host=$mysqlhostname --disable-column-names --user=$mysqlusername --password=$mysqlpassword $mysqldatabase -e"SELECT pinEnabled FROM pinRevision$revision WHERE pinNumberBCM='$PIN'"`
-			if [ "${enabled[$PIN]}" == "1" ]; then
-				if [ ! -d "/sys/class/gpio/gpio$PIN" ]
-				then
-					gpio export $PIN out
-					if [ "$logging" ]; then addLogItem "Enabled Pin $PIN"; fi
-				fi
-			else
-				if [ -d "/sys/class/gpio/gpio$PIN" ]
-				then
-					gpio unexport $PIN
-					if [ "$logging" ]; then addLogItem "Disabled Pin $PIN"; fi
+	for PIN in $pins; do
+		# Enable or Disable pins accordingly.
+		enabled[$PIN]=`echo "SELECT pinEnabled FROM pinRevision$revision WHERE pinNumberBCM='$PIN'" | $mysqlquery`
+		if [ "${enabled[$PIN]}" == "1" ]; then
+			if [ ! -d "/sys/class/gpio/gpio$PIN" ]; then
+				gpio export $PIN out
+				if [ "$logging" ]; then
+					addLogItem "Enabled Pin $PIN"
 				fi
 			fi
-
-			# Skip disabled pins.
+		else
 			if [ -d "/sys/class/gpio/gpio$PIN" ]; then
-
-				# Read Pin Directions.
-				direction[$PIN]=`mysql -B --host=$mysqlhostname --disable-column-names --user=$mysqlusername --password=$mysqlpassword $mysqldatabase -e"SELECT pinDirection FROM pinRevision$revision WHERE pinNumberBCM='$PIN'"`
-				direction2=`cat /sys/class/gpio/gpio$PIN/direction`
-
-				# Read Pin Status'.
-				status[$PIN]=`mysql -B --host=$mysqlhostname --disable-column-names --user=$mysqlusername --password=$mysqlpassword $mysqldatabase -e "SELECT pinStatus FROM pinRevision$revision WHERE pinNumberBCM='$PIN'"`
-				status2=`gpio -g read $PIN`
-
-				# Change Pin Status'.
-				if [ "${direction[$PIN]}" != "$direction2" ]; then
-					gpio -g mode $PIN ${direction[$PIN]}
-					if [ "$logging" ]; then addLogItem "Pin $PIN changed to: ${direction[$PIN]}"; fi
-				fi
-
-				if [ "${status[$PIN]}" != "$status2" ]; then
-					gpio -g write $PIN ${status[$PIN]}
-					if [ "$logging" ]; then addLogItem "Pin $PIN changed to: ${status[$PIN]}"; fi
+				gpio unexport $PIN
+				if [ "$logging" ]; then
+					addLogItem "Disabled Pin $PIN"
 				fi
 			fi
+		fi
+
+		# Skip disabled pins.
+		if [ -d "/sys/class/gpio/gpio$PIN" ]; then
+			# Read pin directions.
+			direction[$PIN]=`echo "SELECT pinDirection FROM pinRevision$revision WHERE pinNumberBCM='$PIN'" | $mysqlquery`
+			direction2=`cat /sys/class/gpio/gpio$PIN/direction`
+
+			# Read pin status'.
+			status[$PIN]=`echo "SELECT pinStatus FROM pinRevision$revision WHERE pinNumberBCM='$PIN'" | $mysqlquery`
+			status2=`gpio -g read $PIN`
+
+			# Change pin status'.
+			if [ "${direction[$PIN]}" != "$direction2" ]; then
+				gpio -g mode $PIN ${direction[$PIN]}
+				if [ "$logging" ]; then
+					addLogItem "Pin $PIN changed to: ${direction[$PIN]}"
+				fi
+			fi
+			if [ "${status[$PIN]}" != "$status2" ]; then
+				gpio -g write $PIN ${status[$PIN]}
+				if [ "$logging" ]; then
+					addLogItem "Pin $PIN changed to: ${status[$PIN]}"
+				fi
+			fi
+		fi
+
+
+		# Enable or Disable MCP23017 pins accordingly.
+		#enabledmcp23017[$PIN]=`echo "SELECT pinEnabled FROM mcp23017 WHERE pinNumber='$PIN'" | $mysqlquery`
+		#if [ "${enabledmcp23017[$PIN]}" == "1" ]; then
+		#	if [ ! -d "/sys/class/gpio/gpio$PIN" ]; then
+		#		#gpio export $PIN out
+		#		if [ "$logging" ]; then
+		#			addLogItem "Enabled mcp23017 pin $PIN"
+		#		fi
+		#	fi
+		#else
+		#	if [ -d "/sys/class/gpio/gpio$PIN" ]; then
+		#		#gpio unexport $PIN
+		#		if [ "$logging" ]; then
+		#			addLogItem "Disabled mcp23017 pin $PIN"
+		#		fi
+		#	fi
+		#fi
+
+		# Skip disabled MCP23017 pins.
+		# TODO: Need to read actual export status here.
+		if [ -d "/sys/class/gpio/gpio$PIN" ]; then
+			# Read pin base.
+			basemcp23017[$PIN]=`echo "SELECT basePin FROM mcp23017 WHERE pinNumber='$PIN'" | $mysqlquery`
+
+			# Read pin base address.
+			addrmcp23017[$PIN]=`echo "SELECT address FROM mcp23017 WHERE pinNumber='$PIN'" | $mysqlquery`
+
+			# Read pin direction.
+			directionmcp23017[$PIN]=`echo "SELECT pinDirection FROM mcp23017 WHERE pinNumber='$PIN'" | $mysqlquery`
+			# TODO: Need to read actual direction here.
+			#directionmcp230172=`gpio -x mcp23017:${basemcp23017[$PIN]}:${addrmcp23017[$PIN}} ? $PIN`
+
+			# Read pin status'.
+			statusmcp23017[$PIN]=`echo "SELECT pinStatus FROM mcp23017 WHERE pinNumber='$PIN'" | $mysqlquery`
+			statusmcp230172=`gpio -x mcp23017:${basemcp23017[$PIN]}:${addrmcp23017[$PIN}} read $PIN`
+
+			# Change Pin Status'.
+			if [ "${direction[$PIN]}" != "$directionmcp230172" ]; then
+				gpio -x mcp23017:${basemcp23017[$PIN]}:${addrmcp23017[$PIN}} mode $PIN ${direction[$PIN]}
+				if [ "$logging" ]; then
+					addLogItem "mcp23017 pin $PIN changed to: ${direction[$PIN]}"
+				fi
+			fi
+			if [ "${status[$PIN]}" != "$statusmcp230172" ]; then
+				gpio -x mcp23017:${basemcp23017[$PIN]}:${addrmcp23017[$PIN}} write $PIN ${status[$PIN]}
+				if [ "$logging" ]; then
+					addLogItem "mcp23017 pin $PIN changed to: ${status[$PIN]}"
+				fi
+			fi
+		fi
 	done
 
-	# Complete Loop.
+	# Complete loop.
 	sleep $waitTime
 done
-}
+} >> /var/log/GPIOServer.log
