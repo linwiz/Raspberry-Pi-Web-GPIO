7ea1d7e57d6929a553bababb0738a7d6513c7f91
Paku- <pawel@4x4.org.pl>
2015-01-12 19:54:26 +0100

    Log paging: initial work

---
+++ ajax/log.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Log</title>
+<body>
+	<div id='log'>
+		<?php
+		include 'get_log.php';
+		?>
+	</div>
+</body>
+</html>
