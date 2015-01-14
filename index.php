d754c8962875e4b419510d448980f7da7115817f
Matthew Motz <mtec007@gmail.com>
2015-01-13 19:58:07 -0500

    fixed mobile icons

--- index.php
+++ index.php
@@ -10,15 +10,15 @@ if ($pi_rev == '' || $MySQLi_Host == '' || $MySQLi_User == '' || $MySQLi_Passwor
 	<title>RPi Web GPIO</title>
 	<link rel="stylesheet" type="text/css" href="./styles/gpio.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=1"/>
-	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-	<link rel="apple-touch-icon" sizes="57x57" href="./apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="./apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon" sizes="76x76" href="./apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="./apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon" sizes="120x120" href="./apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="./apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon" sizes="152x152" href="./apple-touch-icon-152x152.png" /></head>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon-152x152.png" /></head>
 <body>
 	<script type="text/javascript" src="./js/jquery.min.js"></script>
 	<script type="text/javascript" src="./js/scripts.js"></script>
