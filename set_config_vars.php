95adb28d468b5eab116453b0c8ef14031f05d08e
Matthew Motz <mtec007@gmail.com>
2015-01-21 23:18:51 -0500

    Fix error when enableing debug mode

--- set_config_vars.php
+++ set_config_vars.php
@@ -31,23 +31,21 @@ try {
 	}
 
 	// Debug output.
-	if ($_SESSION['debugMode']) {
-		$configVariables = "            <pre>System Wide Config Variables: </pre>\r\n";
-		$configVariables .= "           <pre>\r\n";
-		$configVariables .= "           [piRevision] => " . $_SESSION['piRevision'] . "\r\n";
-		$configVariables .= "           [debugMode] => " . $_SESSION['debugMode'] . "\r\n";
-		$configVariables .= "           [showDisabledPins] => " . $_SESSION['showDisabledPins'] . "\r\n";
-		$configVariables .= "           [enableLogging] => " . $_SESSION['enableLogging'] . "\r\n";
-		$configVariables .= "           [logPageSize] => " . $_SESSION['logPageSize'] . "\r\n";
-		if (isset($_SESSION['username'])) {
-			$configVariables .= "           [username] => " . $_SESSION['username'] . "\r\n";
-		}
-		if (isset($_SESSION['userID'])) {
-			$configVariables .= "           [userID] => " . $_SESSION['userID'] . "\r\n";
-		}
-		$configVariables .= "           [pageType] => " . $_SESSION['pageType'] . "\r\n";
-		$configVariables .= "           </pre>\r\n";
+	$configVariables = "            <pre>System Wide Config Variables: </pre>\r\n";
+	$configVariables .= "           <pre>\r\n";
+	$configVariables .= "           [piRevision] => " . $_SESSION['piRevision'] . "\r\n";
+	$configVariables .= "           [debugMode] => " . $_SESSION['debugMode'] . "\r\n";
+	$configVariables .= "           [showDisabledPins] => " . $_SESSION['showDisabledPins'] . "\r\n";
+	$configVariables .= "           [enableLogging] => " . $_SESSION['enableLogging'] . "\r\n";
+	$configVariables .= "           [logPageSize] => " . $_SESSION['logPageSize'] . "\r\n";
+	if (isset($_SESSION['username'])) {
+		$configVariables .= "           [username] => " . $_SESSION['username'] . "\r\n";
 	}
+	if (isset($_SESSION['userID'])) {
+		$configVariables .= "           [userID] => " . $_SESSION['userID'] . "\r\n";
+	}
+	$configVariables .= "           [pageType] => " . $_SESSION['pageType'] . "\r\n";
+	$configVariables .= "           </pre>\r\n";
 
 	// Set up state icons.
 	$stateIcon = array();
