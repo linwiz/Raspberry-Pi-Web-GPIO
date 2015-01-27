ce70fe4573cbd5d2305750e1d897f4ea69b16d48
Matthew Motz <mmotz@e-motz.com>
2015-01-26 21:07:32 -0500

    Update GPIOServer.conf.sh

--- GPIOServer.conf.sh
+++ GPIOServer.conf.sh
@@ -1,13 +1,14 @@
 ##############################################
 #####  GPIOServer    Configuration file  #####
 ##############################################
+
 # Database connection information.
-dbtype="mysql"
+dbtype="mysql" # mysql is the only type supported at this time.
 dbhostname="127.0.0.1" # Use 127.0.0.1, not localhost.
-dbport="3306"
-dbusername="user"
-dbpassword="pass"
-dbdatabase="gpio"
+dbport="3306" # service port number
+dbusername="user" # service username
+dbpassword="pass" # service password
+dbdatabase="gpio" # service database
 
 # Set pin refresh time.
 waitTime=1
